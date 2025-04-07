db5fb614b042abecf800a32b88bfe273d704be36
keksobot <github+keksobot@htmlacademy.ru>
2025-04-07 15:39:08 +0000

    ✔️ Сборка #3

---
+++ 3/bundle.554b70992c79099021c8.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".trip-main");new TripPresenter(e,eventsModel,destinationsModel,offersModel).init()}();
+//# sourceMappingURL=bundle.554b70992c79099021c8.js.map
