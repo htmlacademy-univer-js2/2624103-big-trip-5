8eb345cfe2b8cd7f69e48542ee19caa10837591f
Egor <pegus74@mail.ru>
2025-06-14 02:37:09 +0500

    Добавленна обработка звездочек, нескольких форм

---
+++ src/presenter/event-presenter.js
@@ -0,0 +1,147 @@
+import EventView from '../view/event-view.js';
+import EventEditView from '../view/event-edit-view.js';
+import { render, replace, remove } from '../render.js';
+
+export default class EventPresenter {
+  #event = null;
+  #eventComponent = null;
+  #eventEditComponent = null;
+  #container = null;
+  #destinationsModel = null;
+  #offersModel = null;
+  #handleDataChange = null;
+  #handleModeChange = null;
+  #mode = 'DEFAULT';
+  #isEditFormOpen = false;
+
+  constructor({ container, destinationsModel, offersModel, onDataChange, onModeChange }) {
+    if (!container) {
+      throw new Error('Container not provided');
+    }
+    this.#container = container;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
+    this.#handleDataChange = onDataChange;
+    this.#handleModeChange = onModeChange;
+  }
+
+  init(event) {
+    this.#event = event;
+
+    const prevEventComponent = this.#eventComponent;
+    const prevEventEditComponent = this.#eventEditComponent;
+
+    this.#eventComponent = new EventView({
+      event: this.#event,
+      destinationsModel: this.#destinationsModel,
+      offersModel: this.#offersModel,
+      onEditClick: this.#handleEditClick,
+      onFavoriteClick: this.#handleFavoriteClick
+    });
+
+    this.#eventEditComponent = new EventEditView(
+      this.#event,
+      this.#destinationsModel,
+      this.#offersModel
+    );
+
+    this.#eventEditComponent.setSubmitHandler(this.#handleFormSubmit);
+    this.#eventEditComponent.setDeleteHandler(this.#handleDeleteClick);
+    this.#eventEditComponent.setCloseHandler(this.#handleCloseClick);
+
+    if (prevEventComponent === null || prevEventEditComponent === null) {
+      render(this.#eventComponent, this.#container);
+    } else {
+      if (this.#mode === 'DEFAULT') {
+        replace(this.#eventComponent, prevEventComponent);
+      }
+
+      if (this.#mode === 'EDITING') {
+        replace(this.#eventEditComponent, prevEventEditComponent);
+      }
+    }
+
+    this.#eventComponent.setRollupClickHandler(this.#handleEditClick);
+    this.#eventComponent.setFavoriteClickHandler(this.#handleFavoriteClick);
+
+    remove(prevEventComponent);
+    remove(prevEventEditComponent);
+  }
+
+  destroy() {
+    remove(this.#eventComponent);
+    remove(this.#eventEditComponent);
+  }
+
+  resetView() {
+    if (this.#isEditFormOpen) {
+      this.#replaceFormToEvent();
+    }
+  }
+
+  #replaceEventToForm = () => {
+  const currentEventElement = this.#eventComponent.element;
+  this.#eventEditComponent = new EventEditView(
+    this.#event,
+    this.#destinationsModel,
+    this.#offersModel
+  );
+  this.#eventEditComponent.setSubmitHandler(this.#handleFormSubmit);
+  this.#eventEditComponent.setDeleteHandler(this.#handleDeleteClick);
+  this.#eventEditComponent.setCloseHandler(this.#handleCloseClick);
+  currentEventElement.replaceWith(this.#eventEditComponent.element);
+  document.addEventListener('keydown', this.#escKeyDownHandler);
+  this.#mode = 'EDITING';
+  this.#isEditFormOpen = true;
+};
+
+  #replaceFormToEvent = () => {
+  if (this.#isEditFormOpen && this.#eventEditComponent) {
+    const newEventComponent = new EventView({
+      event: this.#event,
+      destinationsModel: this.#destinationsModel,
+      offersModel: this.#offersModel,
+      onEditClick: this.#handleEditClick,
+      onFavoriteClick: this.#handleFavoriteClick
+    });
+    this.#eventEditComponent.element.replaceWith(newEventComponent.element);
+    this.#eventComponent = newEventComponent;
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#mode = 'DEFAULT';
+    this.#isEditFormOpen = false;
+    this.#eventEditComponent = null;
+  }
+};
+ #escKeyDownHandler = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    evt.stopPropagation(); 
+    this.#handleCloseClick();
+  }
+};
+
+  #handleEditClick = () => {
+    this.#replaceEventToForm();
+  };
+  
+isEditFormOpen() {
+  return this.#mode === 'EDITING';
+}
+
+  #handleFavoriteClick = (updatedEvent) => {
+    this.#handleDataChange(updatedEvent);
+  };
+
+  #handleFormSubmit = (updatedEvent) => {
+    this.#handleDataChange(updatedEvent);
+    this.#replaceFormToEvent();
+  };
+
+  #handleDeleteClick = (eventId) => {
+    this.#handleDataChange(eventId, true);
+  };
+
+  #handleCloseClick = () => {
+    this.#replaceFormToEvent();
+  };
+}
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,13 +1,13 @@
-
 import FiltersView from '../view/filters-view';
 import SortView from '../view/sort-view';
 import EventEditView from '../view/event-edit-view';
-import EventView from '../view/event-view';
 import TripInfoView from '../view/trip-info-view';
 import NewEventButtonView from '../view/new-event-button-view';
 import EmptyListView from '../view/empty-list-view';
 import {DEFAULT_EVENT} from '../const';
-import {render, replace, RenderPosition} from '../render';
+import EventPresenter from './event-presenter';
+import {render, replace} from '../render';
+
 const SortType = {
   DAY: 'day',
   TIME: 'time',
@@ -15,253 +15,240 @@ const SortType = {
 };
 
 export default class TripPresenter {
-_currentEventComponent = null;
-_currentEditForm = null;
-constructor(containers, eventsModel, destinationsModel, offersModel) {
-  if (!containers.eventsContainer || !containers.mainContainer) {
-    throw new Error('Containers not provided');
+ #newEventForm = null;
+  #eventsModel = null;
+  #destinationsModel = null;
+  #offersModel = null;
+  #eventsContainer = null;
+  #mainContainer = null;
+  #eventPresenters = new Map();
+  #currentSortType = SortType.DAY;
+  #tripInfoComponent = null;
+  #filtersComponent = null;
+  #sortComponent = null;
+  #newEventButtonComponent = null;
+  #currentEventComponent = null;
+  #currentEditForm = null;
+ #isNewEventFormOpen = false;
+  constructor(containers, eventsModel, destinationsModel, offersModel) {
+    if (!containers.eventsContainer || !containers.mainContainer) {
+      throw new Error('Containers not provided');
+    }
+    this.#eventsContainer = containers.eventsContainer;
+    this.#mainContainer = containers.mainContainer;
+    this.#eventsModel = eventsModel; 
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
+    
+   
   }
-  this._eventsContainer = containers.eventsContainer;
-  this._mainContainer = containers.mainContainer;
-  this._eventsModel = eventsModel; 
-  this._destinationsModel = destinationsModel;
-  this._offersModel = offersModel;
   
-    this._currentSortType = SortType.DAY;
-    this._tripInfoComponent = null;
-    this._filtersComponent = null;
-    this._sortComponent = null;
-    this._newEventButtonComponent = null;
-    
-    this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
-    this._handleNewEventClick = this._handleNewEventClick.bind(this);
-    
-
-     this._currentEventComponent = null;
-    this._currentEditForm = null;
+  init() {
+    console.log('Trip container:', this.#eventsContainer);
+    console.log('List element:', this.#eventsContainer.querySelector('.trip-events__list'));
+    console.log('Проверка данных:', {
+      events: this.#eventsModel.getEvents(), 
+      destinations: this.#destinationsModel.getDestinations(),
+      offers: this.#offersModel.getOffers()
+    });
+    this.#renderTripInfo();
+    this.#renderFilters();
+    this.#renderSort();
+    this.#renderEventsList();
+    this.#renderNewEventButton();
   }
-init() {
-  console.log('Trip container:', this._eventsContainer);
-  console.log('List element:', this._eventsContainer.querySelector('.trip-events__list'));
-  console.log('Проверка данных:', {
-    events: this._eventsModel.getEvents(), 
-    destinations: this._destinationsModel.getDestinations(),
-    offers: this._offersModel.getOffers()
+
+#renderEvent(event) {
+  const eventsListElement = this.#eventsContainer.querySelector('.trip-events__list');
+  if (!eventsListElement) return;
+
+  const eventPresenter = new EventPresenter({
+    container: eventsListElement,
+    destinationsModel: this.#destinationsModel,
+    offersModel: this.#offersModel,
+    onDataChange: this.#handleEventChange, 
+    onModeChange: this.#handleModeChange
   });
-  this._renderTripInfo();
-  this._renderFilters();
-  this._renderSort();
-  this._renderEventsList();
-  this._renderNewEventButton();
+  
+  eventPresenter.init(event);
+  this.#eventPresenters.set(event.id, eventPresenter);
 }
 
+  #renderEventsList() {
+    const eventsListElement = this.#eventsContainer.querySelector('.trip-events__list');
+    if (!eventsListElement) {
+      console.error('Events list container not found');
+      return;
+    }
 
-showTestEditForm() {
-  this._clearEventsList();
-  const testEvent = this._eventsModel.getEvents()[0]; 
-  render(
-    new EventEditView(
-      testEvent,
-      this._destinationsModel,
-      this._offersModel
-    ),
-    this._eventsContainer.querySelector('.trip-events__list')
-  );
-}
-  _renderTripInfo() {
-    this._tripInfoComponent = new TripInfoView(
-  this._eventsModel.getEvents(),
-  this._destinationsModel.getDestinations(),
-  this._offersModel.getOffers()
-);
-    const container = this._mainContainer.querySelector('.trip-main__trip-info');
+    eventsListElement.innerHTML = '';
+    const events = this.#getSortedEvents();
+    
+    events.forEach(event => {
+      this.#renderEvent(event);
+    });
+  }
+
+  #renderTripInfo() {
+    const events = this.#eventsModel.getEvents();
+    const destinations = this.#destinationsModel.getDestinations();
+    
+    this.#tripInfoComponent = new TripInfoView(events, destinations);
+    const container = this.#mainContainer.querySelector('.trip-main__trip-info');
+    
     if (!container) {
       console.error('Trip info container not found');
       return;
     }
     
-    const events = this._eventsModel.getEvents();
-    const destinations = this._destinationsModel.getDestinations();
-    
     container.innerHTML = '';
-    this._tripInfoComponent = new TripInfoView(events, destinations);
-    render(this._tripInfoComponent, container);
+    render(this.#tripInfoComponent, container);
   }
 
-  _renderNewEventButton() {
-    this._newEventButtonComponent = new NewEventButtonView();
-    render(this._newEventButtonComponent, this._mainContainer);
-    this._newEventButtonComponent.getElement()
-      .addEventListener('click', this._handleNewEventClick);
-  }
-
-  _renderFilters() {
-  const filtersContainer = this._eventsContainer.querySelector('.trip-controls__filters');
-  if (!filtersContainer) {
-    return;
-  }
-  filtersContainer.innerHTML = '';
-  const filters = [
-    { type: 'everything', name: 'Everything', count: this._eventsModel.getEvents().length },
-    { type: 'future', name: 'Future', count: this._eventsModel.getFutureEvents().length },
-    { type: 'past', name: 'Past', count: this._eventsModel.getPastEvents().length }
-  ];
-  this._filtersComponent = new FiltersView(filters, 'everything');
-  render(this._filtersComponent, filtersContainer); 
-}
-
-  _renderSort() {
-     const sortContainer = this._eventsContainer.querySelector('.trip-events__trip-sort');
-    
-    if (!sortContainer) {
-    return;
-  }
-  sortContainer.innerHTML = '';
-  this._sortComponent = new SortView(this._currentSortType);
-  render(this._sortComponent, sortContainer);
-  }
-
-#escKeyDownHandler = (evt) => {
-    if (evt.key === 'Escape' && this._editFormComponent) {
-      evt.preventDefault();
-      this.#replaceFormToEvent();
+#renderNewEventButton() {
+  try {
+    if (this.#newEventButtonComponent) {
+      remove(this.#newEventButtonComponent);
+      this.#newEventButtonComponent = null;
     }
-  };
-
-  #replaceFormToEvent() {
-    if (this._editFormComponent && this._eventComponent) {
-      replace(this._eventComponent, this._editFormComponent);
-      document.removeEventListener('keydown', this.#escKeyDownHandler);
-      this._editFormComponent = null;
-    }
-  }
-
-  _renderNewEventButton() {
-     const container = this._eventsContainer.querySelector('.trip-main');
-  if (!container) {
-    return;
-  } 
-  this._newEventButtonComponent = new NewEventButtonView();
-  render(this._newEventButtonComponent, container);
-  this._newEventButtonComponent.getElement()
-    .addEventListener('click', this._handleNewEventClick);
-  }
-
-  _handleSortTypeChange(sortType) {
-    this._currentSortType = sortType;
-    this._clearEventsList();
-    this._renderEventsList();
-  }
-
- _handleNewEventClick() {
-  this.#replaceFormToEvent(); 
-    
-    const eventsListElement = this._eventsContainer.querySelector('.trip-events__list');
-    if (!eventsListElement) return;
-    
-    const newEventForm = new EventEditView(
-      DEFAULT_EVENT,
-      this._destinationsModel,
-      this._offersModel
-    );
-    
-    newEventForm.setCloseHandler(() => {
-      this._renderEventsList();
+    this.#newEventButtonComponent = new NewEventButtonView({
+      onClick: this.#handleNewEventClick
     });
+    render(this.#newEventButtonComponent, this.#mainContainer);
+    console.log('New event button rendered successfully');
+  } catch (error) {
+    console.error('Error rendering new event button:', error);
+  }
+}
+  #renderFilters() {
+    const filtersContainer = this.#eventsContainer.querySelector('.trip-controls__filters');
+    if (!filtersContainer) return;
     
-    eventsListElement.innerHTML = '';
-    render(newEventForm, eventsListElement);
-    this._editFormComponent = newEventForm;
-    document.addEventListener('keydown', this.#escKeyDownHandler);
+    filtersContainer.innerHTML = '';
+    const filters = [
+      { type: 'everything', name: 'Everything', count: this.#eventsModel.getEvents().length },
+      { type: 'future', name: 'Future', count: this.#eventsModel.getFutureEvents().length },
+      { type: 'past', name: 'Past', count: this.#eventsModel.getPastEvents().length }
+    ];
+    
+    this.#filtersComponent = new FiltersView(filters, 'everything');
+    render(this.#filtersComponent, filtersContainer); 
   }
 
-  _clearEventsList() {
-   const list = this._eventsContainer.querySelector('.trip-events__list'); 
-  if (list) list.innerHTML = '';
+  #renderSort() {
+    const sortContainer = this.#eventsContainer.querySelector('.trip-events__trip-sort');
+    if (!sortContainer) return;
+    
+    sortContainer.innerHTML = '';
+    this.#sortComponent = new SortView(this.#currentSortType);
+    render(this.#sortComponent, sortContainer);
   }
 
-  _getSortedEvents() {
-  switch (this._currentSortType) {
-    case SortType.TIME:
-      return this._eventsModel.getEventsSortedByTime();
-    case SortType.PRICE:
-      return this._eventsModel.getEventsSortedByPrice();
-    default: 
-      return this._eventsModel.getEventsSortedByDay();
+  #clearEventsList() {
+    this.#eventPresenters.forEach(presenter => presenter.destroy());
+    this.#eventPresenters.clear();
+    
+    const list = this.#eventsContainer.querySelector('.trip-events__list'); 
+    if (list) list.innerHTML = '';
+  }
+
+  #getSortedEvents() {
+    switch (this.#currentSortType) {
+      case SortType.TIME:
+        return this.#eventsModel.getEventsSortedByTime();
+      case SortType.PRICE:
+        return this.#eventsModel.getEventsSortedByPrice();
+      default: 
+        return this.#eventsModel.getEventsSortedByDay();
     }
   }
-    _renderEventsList() {
-  const eventsListElement = this._eventsContainer.querySelector('.trip-events__list');
-  if (!eventsListElement) {
-    console.error('Events list container not found');
-    return;
+
+#handleEventChange = (updatedEvent, isDeleting = false) => {
+  if (isDeleting) {
+    console.log('Deleting event:', updatedEvent); 
+    this.#eventsModel.deleteEvent(updatedEvent);
+  } else {
+    console.log('Updating event:', updatedEvent); 
+    this.#eventsModel.updateEvent(updatedEvent);
   }
-
-  eventsListElement.innerHTML = '';
-  const events = this._getSortedEvents();
-  console.log('Rendering events:', events); 
-
-  events.forEach(event => {
-    const eventComponent = new EventView(event, this._destinationsModel, this._offersModel);
-    render(eventComponent, eventsListElement);
-    const rollupBtn = eventComponent.element.querySelector('.event__rollup-btn');
-    console.log('Rollup button exists:', !!rollupBtn);
-    eventComponent.setRollupClickHandler(() => {
-      console.log('Rollup clicked for event:', event.id);
-      this._openEditForm(event, eventComponent);
-    });
-  });
-}
- _openEditForm(event, eventComponent) {
-  if (this._currentEditForm) {
-    this._replaceFormToEvent();
-  }
-
-   const editFormComponent = new EventEditView(
-    event,
-    this._destinationsModel,
-    this._offersModel
-  );
-
-  editFormComponent.setCloseHandler(() => this._replaceFormToEvent());
-  editFormComponent.setSubmitHandler((updatedEvent) => { 
-    this._handleEventChange(updatedEvent);
-    this._replaceFormToEvent();
-  });
   
-  editFormComponent.setDeleteHandler((eventId) => {
-    this._eventsModel.deleteEvent(eventId);
-    this._renderTripInfo();
-    this._renderEventsList();
-  });
-
-  this._currentEventComponent = eventComponent;
-  this._currentEditForm = editFormComponent;
-
-  replace(editFormComponent, eventComponent);
-  document.addEventListener('keydown', this._escKeyDownHandler);
-}
-
-
-_handleEventChange(updatedEvent) {
-  this._eventsModel.updateEvent(updatedEvent);
-  this._renderTripInfo(); 
-}
-
-_replaceFormToEvent() {
-  if (!this._currentEventComponent || !this._currentEditForm) return;
-  
-  replace(this._currentEventComponent, this._currentEditForm);
-  document.removeEventListener('keydown', this._escKeyDownHandler);
-  this._currentEditForm = null;
-  this._currentEventComponent = null;
-  this._renderEventsList();
-}
-_escKeyDownHandler = (evt) => {
-  if (evt.key === 'Escape' && this._currentEditForm) {
-    evt.preventDefault();
-    this._replaceFormToEvent();
+  const presenter = this.#eventPresenters.get(updatedEvent.id);
+  if (presenter) {
+    presenter.init(updatedEvent);
   }
 };
 
+
+  #handleModeChange = () => {
+    this.#eventPresenters.forEach(presenter => presenter.resetView());
+  };
+
+    #handleSortTypeChange = (sortType) => {
+    this.#currentSortType = sortType;
+    this.#clearEventsList();
+    this.#renderEventsList();
+  };
+  #handleNewEventClick = () => {
+
+    this.#eventPresenters.forEach(presenter => presenter.resetView());
+    
+    const eventsListElement = this.#eventsContainer.querySelector('.trip-events__list');
+    if (!eventsListElement) return;
+    
+    this.#isNewEventFormOpen = true;
+    this.#newEventButtonComponent.setDisabled(true);
+    
+    this.#newEventForm = new EventEditView(
+      DEFAULT_EVENT,
+      this.#destinationsModel,
+      this.#offersModel
+    );
+    
+    this.#newEventForm.setCloseHandler(() => this.#closeNewEventForm());
+    this.#newEventForm.setSubmitHandler((newEvent) => {
+      this.#eventsModel.addEvent(newEvent);
+      this.#closeNewEventForm();
+    });
+    
+    eventsListElement.prepend(this.#newEventForm.element);
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+#escKeyDownHandler = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.preventDefault();
+    
+    if (this.#isNewEventFormOpen) {
+      this.#closeNewEventForm();
+    } else {
+      this.#eventPresenters.forEach(presenter => {
+        if (presenter.isEditFormOpen()) {
+          presenter.resetView();
+        }
+      });
+    }
+  }
+};
+
+
+#closeNewEventForm = () => {
+  if (this.#newEventForm) {
+    this.#newEventForm.removeElement();
+    this.#newEventForm = null;
+    this.#isNewEventFormOpen = false;
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#newEventButtonComponent.setDisabled(false);
+    this.#renderEventsList();
+  }
+};
+
+  #replaceFormToEvent() {
+    if (!this.#currentEventComponent || !this.#currentEditForm) return;
+    
+    replace(this.#currentEventComponent, this.#currentEditForm);
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+    this.#currentEditForm = null;
+    this.#currentEventComponent = null;
+    this.#renderEventsList();
+  }
 }
